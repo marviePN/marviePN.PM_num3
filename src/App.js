6fa53fca407a698c09068be1ffb03f1e1e1eb7e5
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-09-28 20:06:00 +0800

    Pm_num3 commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Dashboard from './Dashboard';
+
+
+class App extends Component {
+   constructor(props) {
+       super(props);
+       this.state = {
+           inputUserName: '',
+           inputPass:'',
+           username: 'UserName',
+           password:'P@ssw0rd',
+           login: false
+       }
+   }
+
+   handleLogin = (e) => {
+       const { inputuserName, username, inputPass, password } = this.state;
+       if (inputuserName === username && inputPass === password) {
+           this.setState({ login: true });
+       } else {
+           this.setState({ user: false });
+           alert("Invalid Credentials either Password or Username didn't match");
+       }
+   }
+   render() {
+       const { login } = this.state;
+       if (!login) {
+           return (
+               <div id = "main">
+                 <h1>LOG IN FORM </h1>
+                 <hr></hr>
+                 <br></br>
+                   UserName : <input type = "text" placeholder="Username"   onChange = {e => this.setState({inputuserName : e.target.value})}></input>
+                   <br></br><br></br>
+                   Password : <input type='password' placeholder="Password" onChange = {e => this.setState({inputPass : e.target.value})}></input>
+                   <br></br><br></br>
+                   <button className = "button1" onClick={e => this.handleLogin(e)}>Login</button>
+                   <br>
+                   </br><br></br>
+               </div>
+               
+           )
+       }
+     
+       else {
+           return(
+               <Dashboard/>
+           )
+       }
+   }
 }
 
 export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,19 @@
+
+import React,{Component} from 'react';
+
+class Dashboard extends Component{
+    constructor(props){
+        super(props)
+        console.log(this.props);
+    }
+    render(){
+        return(
+            <div>
+                    <div> WELCOME !</div>
+                    <div> Hello, {this.props.name} </div>
+            </div>
+        )
+    }
+}
+export default Dashboard;
+
